4a82d1ab686c21e84ab4c34d0395c50b7dd89f7a
Nishchal Singh <nishchalsingh9a@gmail.com>
2025-08-03 23:19:22 +0530

    connect db

--- threatnet/src/App.jsx
+++ threatnet/src/App.jsx
@@ -1,12 +1,46 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
+import UserDashboard from "./pages/UserDashboard";
+import CommanderDashboard from "./pages/CommanderDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import { ProtectedRoute } from "./auth/ProtectedRoute";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Login />} />
+
+      <Route
+        path="/user"
+        element={
+          <ProtectedRoute allowedRoles={["user", "commander", "admin"]}>
+            <UserDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/commander"
+        element={
+          <ProtectedRoute allowedRoles={["commander", "admin"]}>
+            <CommanderDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute allowedRoles={["admin"]}>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* catch-all fallback */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
