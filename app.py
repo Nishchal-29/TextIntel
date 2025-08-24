3bd343a4ba6e091f44c97d9bd570d0194af369d0
Shuvrodip Das <hello.shuvrodip@gmail.com>
2025-08-24 21:00:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -383,7 +383,7 @@ def get_model_metrics():
     return {
         "val_accuracy": val_accuracy,
         "trained_examples": trained_examples,
-        "new_db_examples": 5,
+        "new_db_examples": new_db_examples,
         "training": training,
     }
 
@@ -391,6 +391,7 @@ def get_model_metrics():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # use PORT (Render sets this)
     uvicorn.run("app:app", host="0.0.0.0", port=port)
+
 
 
 
